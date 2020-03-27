2c6672de6e449e5541c2df3c5b29665def3f7a1f
Jaya-shukla-cse <jaya.shukla_cs18@gla.ac.in>
2020-03-28 00:37:04 +0530

    created getters

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -16,4 +16,26 @@ public class Student {
         this.a_count = a_count;
         this.backlog = backlog;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "roll=" + roll +
+                ", backlog=" + backlog +
+                ", a_count=" + a_count +
+                '}';
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public int getA_count() {
+        return a_count;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
 }
+
